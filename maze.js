230ed17d21ec72237434af49f1bd7b57864e9895
na3ksg <superkasago@yahoo.co.jp>
2016-01-29 20:45:19 +0900

    変数の初期化

--- maze.js
+++ maze.js
@@ -1,3 +1,14 @@
 //変数の初期化
+var cvMain;//キャンバス
+var ctx;//描画用コンテキスト
+var width = 33,height = 33;//迷路の広さ
+var maze = [];迷路データ
+var px,py;//プレイヤーの座標
+var keyDir = {//押されたキーの向き
+	38:[0,-1],40:[0,1],//up down
+	37:[-1,0],39:[1,0],//left right
+	75:[0,-1],74:[0,1],//j k
+	72:[-1,0],76:[1,0]//hl
+};
 //ブロックの色
 var blockColor = ["white","black","yellow"];
